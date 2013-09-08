5f55ac598ae2454c86265cdb4495c821672d6c5f
jaymz <jaymz@u-dox.com>
2013-09-08 19:42:00 +0100

    implement penalty functions on knapsack from a research paper

--- m366/knapsack.py
+++ m366/knapsack.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-
+#
 #  :::  .   :::.    :::.  :::.  ::::::::::.  .::::::.   :::.       .,-:::::  :::  .    .::::::.
 #  ;;; .;;,.`;;;;,  `;;;  ;;`;;  `;;;```.;;;;;;`    `   ;;`;;    ,;;;'````'  ;;; .;;,.;;;`    `
 #  [[[[[/'    [[[[[. '[[ ,[[ '[[, `]]nnn]]' '[==/[[[[, ,[[ '[[,  [[[         [[[[[/'  '[==/[[[[,
@@ -11,6 +11,12 @@
 # GA demos and overviews. This code is focused around TMA04 and study work
 # but could be somewhat useful for exploration. As such usual caveats apply
 # about verbosity or unoptimized sections.
+
+from __future__ import division
+import math
+
+# Convinence function for log to base 2
+ln2 = lambda x: math.log(x, 2)
 
 
 class Knapsack(object):
@@ -53,6 +59,66 @@ class Knapsack(object):
         return 'Knapsack %s [Valid: %s] (Value: %d, Weight: %d)' % \
             (self.bitstring, self.valid, self.value, self.weight)
 
+    def fitness(self, penalty):
+        """
+            Returns fitness value with a given penalty function applied
+        """
+        if self.valid:
+            return self.value
+        else:
+            return max(self.value - penalty(self), 0)
+
+
+class Penalty(object):
+
+    def __init__(self):
+        pass
+
+    def rho(self, values, weights):
+        """
+            Returns scaling constant based on Michalewicz and Arabas (1994) schemes
+        """
+        # Element by element division and then select the max value of this
+        return max([values[i] / weights[i] for i in range(len(values))])
+
+    # The following penalty functions are defined in the paper:
+    #
+    # Michalewicz, Z. and Arabas, J. (1994) Genetic algorithms for the 0/1
+    # knapsack problem, Lecture Notes in Computer Science, vol. 869, pp. 134 -
+    # 143, DOI 10.1007/3-540-58495-1_14.
+    #
+    # and are used as is
+
+    @property
+    def ap1(self):
+
+        def penalty(knapsack):
+            rho = self.rho(knapsack.values, knapsack.weights)
+            return ln2(1 + rho * abs(knapsack.weight - knapsack.capacity))
+
+        return penalty
+
+    @property
+    def ap2(self):
+        """
+            Returns a function that increases linearly with excess weight
+        """
+
+        def penalty(knapsack):
+            rho = self.rho(knapsack.values, knapsack.weights)
+            return rho * (knapsack.weight - knapsack.capacity)
+
+        return penalty
+
+    @property
+    def ap3(self):
+
+        def penalty(knapsack):
+            rho = self.rho(knapsack.values, knapsack.weights)
+            return (rho * (knapsack.weight - knapsack.capacity)) ** 2
+
+        return penalty
+
 
 if __name__ == '__main__':
 
@@ -62,6 +128,14 @@ if __name__ == '__main__':
     sack = Knapsack(values, weights)
     sacks = ['000111', '010110', '100100', '111111']
 
+    penalty = Penalty()
+
     for bitstring in sacks:
         sack.bitstring = bitstring
+
         print sack
+        print "Fitness: AP1: %0.4f, AP2: %0.4f, AP3: %0.4f\n" % (
+            sack.fitness(penalty.ap1),
+            sack.fitness(penalty.ap2),
+            sack.fitness(penalty.ap3),
+        )
